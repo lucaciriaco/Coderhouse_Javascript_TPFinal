71358ba45ca389d93bb1db32cd2d87f9ffbd1195
lucaciriaco <33738825+lucaciriaco@users.noreply.github.com>
2022-04-08 21:01:37 -0300

    .

--- js/app.js
+++ js/app.js
@@ -1,43 +1,70 @@
 class Quiz {
-    constructor(type,questions,correctNumber){
+    constructor(type,question,answer1,answer2,answer3,answer4,correctNumber){
         //El tipo seria diferente categoria de preguntas que voy a agregar mas tarde
         this.type = type;
-        this.questions = questions;
+        this.question = question;
+        this.answer1 = answer1;
+        this.answer2 = answer2;
+        this.answer3 = answer3;
+        this.answer4 = answer4;
         this.correctNumber = correctNumber;
     }
 }
 
-//Las preguntas me gustaria guardarlas en un json.
-let question1 = new Quiz(0,"¿Quién pinto 'La Gioconda'? <br> [ 0 ] Leonardo Da Vinci <br> [ 1 ] Rafael Sanzio <br> [ 2 ] Donatello <br> [ 3 ]" ,0);
-let question2 = new Quiz(0,"",3);
-let question3 = new Quiz(0,"",2);
-let question4 = new Quiz(0,"",1);
-let questionsArray = new Array(3);
-questionsArray[0] = question1;
-questionsArray[1] = question2;
-questionsArray[2] = question3;
-questionsArray[3] = question4;
+//
+let question = document.getElementById("question");
+let btnAnswer1 = document.getElementById("answer1");
+let btnAnswer2 = document.getElementById("answer2");
+let btnAnswer3 = document.getElementById("answer3");
+let btnAnswer4 = document.getElementById("answer4");
+let topScoreard = document.getElementById("topScore");
+let scoreBoard = document.getElementById("Score");
 
-function gameManager(questionsArray){
+
+//Le agrego el evento a los botones
+btnAnswer1.addEventListener('click',() =>{awnser = 1})
+btnAnswer2.addEventListener('click',() =>{awnser = 2})
+btnAnswer3.addEventListener('click',() =>{awnser = 3})
+btnAnswer4.addEventListener('click',() =>{awnser = 4})
+
+
+//Las preguntas me gustaria guardarlas en un json.
+let question1 = new Quiz(0,"Quién pinto 'La Gioconda'?",'Leonardo Da Vinci','','','',0);
+let question2 = new Quiz(0,"Quién pinto 'La Gioconda'?",'Leonardo Da Vinci','','','',0);
+let question3 = new Quiz(0,"Quién pinto 'La Gioconda'?",'Leonardo Da Vinci','','','',0);
+let question4 = new Quiz(0,"Quién pinto 'La Gioconda'?",'Leonardo Da Vinci','','','',0);
+let questionsArray = new Array(3);
+questionsArray.push('question1','question2','question3','question4');
+
+console.log(questionsArray.length);
+
+window.onload = gameManager();
+
+function gameManager(questionsArray, answer, topScore, scoreBoard){
     let score = 0;
     let win = true;
     do{
-        let random = Math.floor(Math.random() * questionsArray.length);
-        document.write(questionsArray[random].questions);
-        do{
-            let awnser = Number(prompt("Ingrese numero de respuesta numerica"));
-        }while(isNaN(awnser) && awnser < 3)
-        if(awnser == questionsArray[random].correctNumber)
-        {
+        let randomQuiz = Math.floor(Math.random() * questionsArray.length);
+        btnAnswer1.innerHTML(randomQuiz.answer1);
+        btnAnswer2.innerHTML(randomQuiz.answer2);
+        btnAnswer3.innerHTML(randomQuiz.answer3);
+        btnAnswer4.innerHTML(randomQuiz.answer4);
+        if(answer == questionsArray[randomQuiz].correctNumber)
+        {   
+            console.log("Correct answer!")
             win = true
+            scoreBoard.innerHTML("Puntaje: " + score);
             score++;
-        }else
+        }
+        else
         {
+            console.log("Wrong answer!")
             win = false;
             gameOver(null,score);
         }
     }while(win == true);
 }
+
 
 //El best score lo voy a agregar cuando sepa guardar cosas en json;
 function gameOver(bestScore,score){
@@ -47,7 +74,9 @@ function gameOver(bestScore,score){
     document.write("<h6>Fin del juego el puntaje final es: </h6>", + score,"<br><h6>El mejor puntaje es: </h6>" + bestScore);
     if (confirm("Desea jugar otra vez")==true){
         gameManager();
-    }else{
+    }
+    else
+    {
         document.write("<br><h6>Hecho por Luca Ciriaco para <a href='https://www.coderhouse.com/'>Coderhouse</a></h6>");
     }
 }
