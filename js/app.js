4baed9014072f74a41696dd1d134d28d90ea5004
lucaciriaco <33738825+lucaciriaco@users.noreply.github.com>
2022-04-23 19:01:32 -0300

    .

--- js/app.js
+++ js/app.js
@@ -1,83 +1,125 @@
 class Quiz {
-    constructor(type,question,answer1,answer2,answer3,answer4,correctNumber){
+    constructor(type,question,answer,correctNumber){
         //El tipo seria diferente categoria de preguntas que voy a agregar mas tarde
         this.type = type;
         this.question = question;
-        this.answer1 = answer1;
-        this.answer2 = answer2;
-        this.answer3 = answer3;
-        this.answer4 = answer4;
+        this.answer = answer;
         this.correctNumber = correctNumber;
     }
 }
 
-//
+// Variable global que va a guardar la respuesta de los botones.
+var answer = null;
+let score = 0;
+let win = true;
+let highScore =loadHighScore();
+const quizJson='[{"type":"0","question":"Pregunta 1","answer":["1","2","3","4"],"correctNumber":"1"},{"type":"0","question":"Pregunta 2","answer":["2","3","4","1"],"correctNumber":"2"},{"type":"0","question":"Pregunta 3","answer":["3","4","1","2"],"correctNumber":"3"},{"type":"0","question":"Pregunta 4","answer":["4","1","2","3"],"correctNumber":"4"}]'
+var quizArray = JSON.parse(quizJson);
+let actualQuiz = {};
+// Guardo los elementos del dom.
 let question = document.getElementById("question");
-let btnAnswer1 = document.getElementById("answer1");
-let btnAnswer2 = document.getElementById("answer2");
-let btnAnswer3 = document.getElementById("answer3");
-let btnAnswer4 = document.getElementById("answer4");
+let startBtn = document.getElementById("startBtn");
+let btns = document.getElementsByClassName("answer");
 let topScoreard = document.getElementById("topScore");
-let scoreBoard = document.getElementById("Score");
-
+let scoreBoard = document.getElementById("score");
 
 //Le agrego el evento a los botones
-btnAnswer1.addEventListener('click',() =>{awnser = 1})
-btnAnswer2.addEventListener('click',() =>{awnser = 2})
-btnAnswer3.addEventListener('click',() =>{awnser = 3})
-btnAnswer4.addEventListener('click',() =>{awnser = 4})
+btns[0].addEventListener('click',() =>{answer = 1;gameManager()})
+btns[1].addEventListener('click',() =>{answer = 2;gameManager()})
+btns[2].addEventListener('click',() =>{answer = 3;gameManager()})
+btns[3].addEventListener('click',() =>{answer = 4;gameManager()})
+startBtn.addEventListener('click',() => startGame())
+startGame()
 
-
-//Las preguntas me gustaria guardarlas en un json.
-let question1 = new Quiz(0,"Quién pinto 'La Gioconda'?",'Leonardo Da Vinci','','','',0);
-let question2 = new Quiz(0,"Quién pinto 'La Gioconda'?",'Leonardo Da Vinci','','','',0);
-let question3 = new Quiz(0,"Quién pinto 'La Gioconda'?",'Leonardo Da Vinci','','','',0);
-let question4 = new Quiz(0,"Quién pinto 'La Gioconda'?",'Leonardo Da Vinci','','','',0);
-let questionsArray = new Array(3);
-questionsArray.push('question1','question2','question3','question4');
-
-console.log(questionsArray.length);
-
-window.onload = gameManager();
-
-function gameManager(questionsArray, answer, topScore, scoreBoard){
-    let score = 0;
-    let win = true;
-    do{
-        let randomQuiz = Math.floor(Math.random() * questionsArray.length);
-        btnAnswer1.innerHTML(randomQuiz.answer1);
-        btnAnswer2.innerHTML(randomQuiz.answer2);
-        btnAnswer3.innerHTML(randomQuiz.answer3);
-        btnAnswer4.innerHTML(randomQuiz.answer4);
-        if(answer == questionsArray[randomQuiz].correctNumber)
-        {   
-            console.log("Correct answer!")
-            win = true
-            scoreBoard.innerHTML("Puntaje: " + score);
-            score++;
-        }
-        else
-        {
-            console.log("Wrong answer!")
-            win = false;
-            gameOver(null,score);
-        }
-    }while(win == true);
+function startGame(){
+    console.log("start Game")
+    win=true;
+    score=0;
+    topScoreard.innerText=loadHighScore();
+    showButtons();
+    loadQuiz();
+    
 }
 
-
-//El best score lo voy a agregar cuando sepa guardar cosas en json;
-function gameOver(bestScore,score){
-    if(score>bestScore){
-        bestScore = score;
-    }
-    document.write("<h6>Fin del juego el puntaje final es: </h6>", + score,"<br><h6>El mejor puntaje es: </h6>" + bestScore);
-    if (confirm("Desea jugar otra vez")==true){
-        gameManager();
-    }
-    else
+function loadQuiz(){
+    if(win==true)
     {
-        document.write("<br><h6>Hecho por Luca Ciriaco para <a href='https://www.coderhouse.com/'>Coderhouse</a></h6>");
+        let random = loadRandomQuiz();
+        actualQuiz = quizArray[random];
+        question.innerText = actualQuiz.question;
+        btns[0].innerText=actualQuiz.answer[0]
+        btns[1].innerText=actualQuiz.answer[1]
+        btns[2].innerText=actualQuiz.answer[2]
+        btns[3].innerText=actualQuiz.answer[3]
+    }
+    
+}
+
+function gameManager(){
+    if(win==true)
+    {
+        if(answer == actualQuiz.correctNumber){   
+            win = true
+            score++;
+            scoreBoard.innerHTML= "Puntaje: " + score;
+            if(score>highScore){
+                saveHighScore(score);
+            }
+        }
+        else{
+            win = false;
+            hideButtons();
+            topScoreard.innerText=loadHighScore();
+        }
+        loadQuiz();
     }
 }
 
+
+
+function loadRandomQuiz(){
+    let randomQuizObject =Math.floor(Math.random() * quizArray.length);
+    return randomQuizObject;
+}
+
+function loadHighScore(){
+    let highScore = localStorage.getItem("highScore");
+    if(highScore==null){
+        highScore = 0;
+    }
+    return highScore;
+}
+
+function saveHighScore(newHighScore){
+    localStorage.setItem("highScore",newHighScore);
+}
+
+function showAnswers(btns,quiz){
+    for(i = 0; i < btns.length ; i++){
+        btns[i].innerText = quiz.answer[i];
+    }
+}
+
+function loadQuizJson(){
+    //arreglar luego
+}
+
+function showButtons(){
+    for(i = 0; i < btns.length; i++){
+        btns[i].hidden = false;
+    }
+
+    startBtn.hidden=true;
+}
+
+function hideButtons(){
+    for(i = 0; i < btns.length; i++){
+        btns[i].hidden = true;
+    }
+
+    startBtn.hidden=false;
+}
+
+function gameOver(){
+
+}
