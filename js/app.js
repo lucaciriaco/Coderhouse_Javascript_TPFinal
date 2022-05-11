bad93bb00c4c18999bef38845342fbc59fe52ea7
lucaciriaco <33738825+lucaciriaco@users.noreply.github.com>
2022-05-11 06:30:30 -0300

    a

--- js/app.js
+++ js/app.js
@@ -8,12 +8,18 @@ let question = document.getElementById("question");
 //Variables globales
 let score = 0;
 let win;
-let highScore =loadHighScore();
+let highScore = loadHighScore();
 let answer;
 let msj;
 
+//Variables de audio
+let backgroundMusic;
+let correctAudio = new Audio('./audio/correct.mp3');
+let incorrectAudio = new Audio('./audio/incorrect.mp3');
+
 //Antes que nada ocultamos los botones
 hideButtons();
+
 //Agrego los eventos a los botones pasando por parametro el indice del boton
 btns[0].addEventListener('click',() =>{gameManager(0)})
 btns[1].addEventListener('click',() =>{gameManager(1)})
@@ -23,7 +29,8 @@ startBtn.addEventListener('click',() => startGame())
 
 //Se ejecuta con el boton startbtn reinicia las variables y actualiza el puntaje
 function startGame(){
-    score=0;
+    score = 0;
+    question.hidden = false;
     scoreBoard.innerHTML= "Puntaje: " + score;
     topScoreard.innerText=loadHighScore();
     showButtons();
@@ -32,19 +39,19 @@ function startGame(){
 
 //Carga completamente al azar una pregunta desde la API y la muestra en el DOM
 function loadQuiz(){
-    fetch('https://opentdb.com/api.php?amount=1')
+    fetch('https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple')
         .then((response) => response.json())
         .then((data) => {
-            const quiz=data.results[0]
-            question.innerText=quiz.question;
+            const quiz = data.results[0]
+            question.innerText = quiz.question;
             const random = Math.floor(Math.random() * 4);
-            let incorrectIndex=0;
+            let incorrectIndex = 0;
             for (let index = 0; index < 4; index++) {
                 if(index==random){
                     console.log(quiz.correct_answer)
                     btns[index].innerText = quiz.correct_answer;
-                    answer=index;
-                    msj=quiz.correct_answer;
+                    answer = index;
+                    msj = quiz.correct_answer;
                 }
                 else{
                     btns[index].innerText = quiz.incorrect_answers[incorrectIndex];
@@ -65,21 +72,22 @@ function gameManager(value){
         if(score>highScore){
             saveHighScore(score);
         }
-        gameOver('Correct!','success');
+        alertPopUp('Correct!','success');
+        correctAudio.play();
         loadQuiz();
     }
     else{
-        gameOver(msj,'error');
+        alertPopUp(msj,'error');
+        incorrectAudio.play();
         hideButtons();
         topScoreard.innerText=loadHighScore();
     } 
 }
 
-
 //Carga el puntaje mas alto almacenado desde localstorage de no existir lo inicia en 0
 function loadHighScore(){
     let highScore = localStorage.getItem("highScore");
-    if(highScore==null){
+    if(highScore == null){
         highScore = 0;
     }
     return highScore;
@@ -95,7 +103,8 @@ function showButtons(){
     for(i = 0; i < btns.length; i++){
         btns[i].hidden = false;
     }
-    startBtn.hidden=true;
+    startBtn.hidden = true;
+    question.hidden = false;
 }
 
 //Muestra el boton de empezar el juego y oculta los botones de las respuestas
@@ -103,13 +112,14 @@ function hideButtons(){
     for(i = 0; i < btns.length; i++){
         btns[i].hidden = true;
     }
-    startBtn.hidden=false;
+    startBtn.hidden = false;
+    question.hidden = true;
 }
 
 //Crea una alerta con la libreria SweetAlertJS
-function gameOver(correct_answer,icon){
+function alertPopUp(correct_answer,icon){
     Swal.fire({
-        text: 'The correct answer was: '+correct_answer,
+        title: '<strong>The correct answer was:</strong> <br>'+ correct_answer,
         icon:icon
     });
 }
